366b72af5ea9a5a5e8f1c31f3979e91070b11dfa
Martin Lindhe <martin@ubique.se>
2016-01-22 22:40:37 +0100

    added ReadAsUTF8(r io.Reader)

--- encoding_test.go
+++ encoding_test.go
@@ -1,12 +1,23 @@
 package subtitles
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLooksLikeLatin1(t *testing.T) {
+
 	assert.Equal(t, true, looksLikeLatin1([]byte("hall\xe5")))
 	assert.Equal(t, false, looksLikeLatin1([]byte("hallå")))
+}
+
+func TestReadFileAsUTF8(t *testing.T) {
+
+	f, err := os.Open("README.md")
+	assert.Equal(t, nil, err)
+
+	_, err = ReadAsUTF8(f)
+	assert.Equal(t, nil, err)
 }
